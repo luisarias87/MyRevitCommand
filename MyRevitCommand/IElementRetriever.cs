0f19c32035cb8d0d1ea66574c37162df34705c10
luisarias87 <ariasluisla89@gmail.com>
2023-03-23 15:57:00 -0500

    Add project files.

---
+++ MyRevitCommand/IElementRetriever.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.Creation;
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace MyRevitCommand
+{
+    public interface IElementRetriever
+    {
+        IList<Element> GetElements(Document document);
+    }
+}
---
+++ MyRevitCommand/MyCommand.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+
+namespace MyRevitCommand
+{
+    [Transaction(TransactionMode.Manual)]
+    public class MyCommand : IExternalCommand
+    {
+        
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            
+
+            var uiApp = commandData.Application;
+            var app  = uiApp.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            var collector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_GenericModel).ToList();
+
+            TaskDialog.Show("123", collector.Count.ToString());
+
+            var window = new UserControl1();
+
+            window.Show();
+            
+                
+
+            return Result.Succeeded;
+        }
+    }
+}
